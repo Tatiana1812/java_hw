a2bb4be1ee953224802e2827c31117f646987ee5
Tatiana1812 <Mukhin657@mail.ru>
2025-08-19 10:47:36 +0300

    hw07_patterns

--- hw07_patterns/src/main/java/ru/otus/listener/homework/HistoryListener.java
+++ hw07_patterns/src/main/java/ru/otus/listener/homework/HistoryListener.java
@@ -11,25 +11,20 @@ public class HistoryListener implements Listener, HistoryReader {
 
     @Override
     public void onUpdated(Message msg) {
-        List<String> field13Old = msg.getField13().getData();
-        List<String> field13New = new ArrayList<>(field13Old.size());
-        field13New.addAll(field13Old);
-        var field13 = new ObjectForMessage();
-        field13.setData(field13New);
-        lst.add(new Message.Builder(msg.getId())
-                        .field1(msg.getField1())
-                        .field2(msg.getField2())
-                        .field3(msg.getField3())
-                        .field4(msg.getField4())
-                        .field5(msg.getField5())
-                        .field6(msg.getField6())
-                        .field7(msg.getField7())
-                        .field8(msg.getField8())
-                        .field9(msg.getField9())
-                        .field10(msg.getField10())
-                        .field11(msg.getField11())
-                        .field12(msg.getField12())
-                        .field13(field13).build());
+        ObjectForMessage oldField13 = msg.getField13();
+        if (oldField13 != null) {
+            List<String> field13Old = oldField13.getData();
+
+            List<String> field13New = new ArrayList<>();
+            field13New.addAll(field13Old);
+
+            var field13 = new ObjectForMessage();
+            field13.setData(field13New);
+
+            lst.add(msg.toBuilder().field13(field13).build());
+        } else {
+            lst.add(msg.toBuilder().build());
+        }
     }
     @Override
     public Optional<Message> findMessageById(long id) {
