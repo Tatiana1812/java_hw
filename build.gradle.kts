12dde2f25af05d72a2c83aa0cd761a3fece9e496
Tatiana1812 <Mukhin657@mail.ru>
2026-01-13 19:14:10 +0300

    hw18_webflux

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ allprojects {
     group = "org.example"
 
     repositories {
+        mavenLocal()
         mavenCentral()
     }
 
@@ -30,6 +31,14 @@ allprojects {
     val testcontainersBom: String by project
     val protobufBom: String by project
     val grpc: String by project
+
+    val sockjs: String by project
+    val stomp: String by project
+    val bootstrap: String by project
+    val jsr305: String by project
+    val wiremock: String by project
+    val r2dbcPostgresql: String by project
+
 
     apply(plugin = "io.spring.dependency-management")
     dependencyManagement {
@@ -43,6 +52,12 @@ allprojects {
             dependency("io.grpc:grpc-netty:$grpc")
             dependency("io.grpc:grpc-protobuf:$grpc")
             dependency("io.grpc:grpc-stub:$grpc")
+            dependency("org.webjars:sockjs-client:$sockjs")
+            dependency("org.webjars:stomp-websocket:$stomp")
+            dependency("org.webjars:bootstrap:$bootstrap")
+            dependency("com.github.tomakehurst:wiremock-standalone:$wiremock")
+            dependency("io.r2dbc:r2dbc-postgresql:$r2dbcPostgresql")
+            dependency("com.google.code.findbugs:jsr305:$jsr305")
         }
     }
 
